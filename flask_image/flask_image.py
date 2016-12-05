daaa69ab831330262833a541314a617efc8a470a
hadxu <hadxu123@gmail.com>
2016-12-05 20:29:24 +0800

    添加照片展示功能

--- flask_image/flask_image.py
+++ flask_image/flask_image.py
@@ -4,6 +4,7 @@ import os
 from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
+app.secret_key = "121234123412341234123412341234"
 
 
 @app.route('/')
@@ -13,13 +14,12 @@ def hello_world():
 
 @app.route('/<filename>')
 def send_image(filename):
-    return send_from_directory(app.static_folder+'\images', filename)
+    return send_from_directory(app.static_folder + '\images', filename)
 
 
 @app.route('/gallery')
 def get_gallery():
-    image_names = os.listdir(app.static_folder+'\images')
-    print image_names
+    image_names = os.listdir(app.static_folder + '\images')
     return render_template('gallery.html', image_names=image_names)
 
 
